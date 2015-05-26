cead345c0d856de6da6222a1f73852fa6b177f0d
David Flower <dflower@amplience.com>
2015-05-26 20:08:32 +0100

    latest is greatest

--- spec/classes/charles_spec.rb
+++ spec/classes/charles_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe 'charles' do
-  let(:params) { {:version => '3.9.3'} }
+  let(:params) { {:version => '3.10.1'} }
 
   it { should contain_class('charles') }
   it { should contain_package('Charles').with_provider('appdmg_eula_charles') }
-  it { should contain_package('Charles').with_source('http://www.charlesproxy.com/assets/release/3.9.3/charles-proxy-3.9.3-applejava.dmg') }
+  it { should contain_package('Charles').with_source('http://www.charlesproxy.com/assets/release/3.10.1/charles-proxy-3.10.1-applejava.dmg') }
 
 end
